ee33ca85901c03f2c2daac97fa8c57f507c5f1a9
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-06 14:28:03 -0800

    fetchについての学習

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import './App.css'
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
 
   useEffect(() => {
     const fetchPokemonData = async () => {
-      let res = await hetAllPokemon(initialURL);
-    }
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
   },[])
 
   return (
