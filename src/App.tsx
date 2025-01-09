4edf2b51aadcb6408771f231e557836adadb772a
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-09 10:04:43 -0800

    header追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import { getAllPokemon, getPokemon, Pokemon } from './utils/pokemon';
-import Card from './components/card/Card';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getAllPokemon, getPokemon, Pokemon } from "./utils/pokemon";
+import Card from "./components/card/Card";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState<Pokemon[]>([]);
 
@@ -17,9 +18,11 @@ function App() {
       setLoading(false);
     };
     fetchPokemonData();
-  },[]);
+  }, []);
 
-  const loadPokemon = async (data: { name: string; url: string }[]): Promise<void> => {
+  const loadPokemon = async (
+    data: { name: string; url: string }[]
+  ): Promise<void> => {
     let _pokemonData = await Promise.all(
       data.map((pokemon) => getPokemon(pokemon.url))
     );
@@ -29,18 +32,23 @@ function App() {
   console.log(pokemonData);
 
   return (
-    <div className='App'>
-      {loading ? (
-        <h1>ロード中・・・</h1>
-      ) : (<>
-      <div className="pokemonCardContainer">
-        {pokemonData.map((pokemon: Pokemon, i: number) => {
-          return <Card key={i} pokemon={pokemon} />
-        })}
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>ロード中・・・</h1>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon: Pokemon, i: number) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+          </>
+        )}
       </div>
-      </>)}
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
